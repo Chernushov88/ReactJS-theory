6137c6a4a8e5a47360063d80b569fad707f89e63
Chernushov88 <Chernushov88@mail.ru>
2019-09-20 12:39:47 +0300

    create props and their syntax

--- src/test.js
+++ src/test.js
@@ -1,31 +1,35 @@
-import React, {PropTypes, Component} from 'react';
+import React, {PropTypes} from 'react';
 import ReactDom from 'react-dom';
 
-class App extends Component{
+class App extends React.Component{
     static propTypes ={
         btnText: PropTypes.string.isRequired,
         h1text: PropTypes.string.isRequired,
-        newArray: PropTypes.array.isRequired
+        newArray: PropTypes.string.isRequired
     };
+
     static defaultProps ={
         btnText: 'default props test'
     };
+
     bntOnClick(event) {
         console.log('Button on click', event);
     }
+
     render() {
         console.log('array', this.props.newArray);
         return(
-            <div className="test" >
+            <div className="test" style={{backgroundColor: 'red'}} >
                 <h1>{ this.props.h1text }</h1>
                 <h3>it's really works!!!!</h3>
-                <button onClick={this.bntOnClick}>{this.props.btnText || 'Default btn text'}</button>
+                <button onClick={this.bntOnClick}>{this.props.btnText}</button>
             </div>
 
         );
     }
 }
+
 ReactDom.render(
-    <App  h1text="this is h1 text" newArray={[1,2,3,4,5]}/>,
-    document.querySelector('#app')
+    <App /> btnText="click on me!" h1text="this is h1 text" newArray={[1,2,3,4,5]},
+document.querySelector('#app')
 );
