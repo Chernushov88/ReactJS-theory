965a01da1f3e0c9e915c9ed083034a0e804ff28e
Chernushov88 <Chernushov88@mail.ru>
2019-09-20 14:32:57 +0300

    Communication life cicle.

--- src/index.js
+++ src/index.js
@@ -6,12 +6,22 @@ class App extends Component{
 
     constructor(props) {
         super(props);
+
+        this.state = {
+            timer: 0
+        };
+    }
+    componentWillMount(){
+        setInterval(() => {
+            this.setState({ timer: this.state.timer+1 });
+        }, 500);
     }
     render() {
         return(
             <div>
                 <h1>App componrnt</h1>
-                <NewComponent text="hellow from app" />
+                { this.state.timer < 10 ? <NewComponent text="hellow from app" /> : null}
+                <p>{ this.state.timer }</p>
            </div>
 
         );
--- src/new.js
+++ src/new.js
@@ -4,8 +4,20 @@ export default class NewComponent extends Component{
 
     constructor(props) {
         super(props);
+        console.log("Constructor");
     }
+    componentWillMount() {
+        console.log("ComponentWillMount");
+    }
+    componentDidMount() {
+        console.log("ComponentDidMount");
+    }
+    componentWillUnmount() {
+        console.log("componentWillUnmount");
+    }
+
     render() {
+        console.log("render");
         return(
             <div style={{backgroundColor: 'red'}}>
                 <h3>New Componrnt</h3>
