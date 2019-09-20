6445c85ed89dad7e13288d450c3ffd101003a50d
Chernushov88 <Chernushov88@mail.ru>
2019-09-20 11:32:35 +0300

    create first component JSX sintax

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
-console.log('Hi !!!!!');
+import React from 'react';
+import ReactDom from 'react-dom';
+
+class App extends React.Component{
+
+    bntOnClick(event) {
+        console.log('Button on click', event);
+    }
+
+    render() {
+        return(
+            <div className="test" style={{backgroundColor: 'red'}} >
+                <h1>App works!</h1>
+                <h3>it's really works!!!!</h3>
+                <button onClick={this.bntOnClick}>{this.props.children}</button>
+            </div>
+
+        );
+    }
+
+}
+
+ReactDom.render(
+    <App> test props</App>,
+    document.querySelector('#app')
+);
